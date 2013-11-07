6e73b7a20db2853dc97e903736c4d89cb9f56722
Marc Bleeze <marcbleeze@gmail.com>
2013-11-07 22:56:18 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PerfPlot::Application.config.secret_key_base = '279d748835801017c496579a9bee6e8d34d7bfe771f5ca0110e2805f5554c340f9315695b085b9570d08845f1cf190e127c0b81e1a4ad128363a6de718657b55'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PerfPlot::Application.config.session_store :cookie_store, key: '_PerfPlot_session'
